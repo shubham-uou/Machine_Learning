33d86c67b4b156fc1847b8672306239e958c7ea2
shubham-uou <u1526846@utah.edu>
2024-11-26 21:36:03 -0700

    Create run1.sh

---
+++ Perceptron/run1.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "Standard Perceptron"
+python3 standard_perceptron.py
+
+echo "Voted Perceptron"
+python3 voted_perceptron.py
+
+echo "Average Perceptron"
+python3 average_perceptron.py
